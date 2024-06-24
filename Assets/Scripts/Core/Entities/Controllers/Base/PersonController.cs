0255c60f0df598ab6ff83992d6476bfdd2f0ceb4
lsd7nk <87329383+lsd7nk@users.noreply.github.com>
2024-06-25 03:18:06 +0700

    minor edits in PersonController.cs

--- Assets/Scripts/Core/Entities/Controllers/Base/PersonController.cs
+++ Assets/Scripts/Core/Entities/Controllers/Base/PersonController.cs
@@ -1,9 +1,12 @@
+using UnityEngine.AI;
 using UnityEngine;
 
 namespace Core.Entities
 {
     public abstract class PersonController : MonoBehaviour, IMovable, IRotatable
     {
+        [SerializeField] private NavMeshAgent _agent;
+
         public void Move(Vector3 direction)
         {
 
